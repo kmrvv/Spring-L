f13def3e59f94ed3d28a2b22ecb6ce045d2f8393
Komarov <kkmrvvv@gmail.com>
2024-11-13 23:59:33 +0300

    Spring Framework: first application

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -11,5 +11,6 @@ public class TestSpring {
         TestBean testBean = context.getBean("testBean", TestBean.class);
         System.out.println(testBean.getName());
         context.close();
+
     }
 }
